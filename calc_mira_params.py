fad4e7099a5c351b80c70ac7804a0591dfad1d29
Pablo <pgarcgo@bloque64.com>
2019-07-10 13:35:45 +0200

    latest

--- calc_mira_params.py
+++ calc_mira_params.py
@@ -9,8 +9,10 @@ import resource
 import subprocess
 import os
 import pwd
+import sys
 
-max_of_open_files =  resource.getrlimit(resource.RLIMIT_NOFILE)[0]
+
+max_nr_of_open_files =  resource.getrlimit(resource.RLIMIT_NOFILE)[0]
 
 
 '''
@@ -43,6 +45,8 @@ Example database.cfg
 
 '''
 bytes_per_gb = 1024*1024*1024
+open_files_offset = 500
+steemd_memory_need = 5.5 * bytes_per_gb
 config_file_name = "database.cfg"
 
 mem = virtual_memory()
@@ -54,10 +58,15 @@ available_ram = mem.available
 
 print("Total RAM: %0.2f Gb" % ((max_ram)/(bytes_per_gb)))
 print("Avaliable RAM: %0.2f Gb" % (available_ram/bytes_per_gb))
+print("Average RAM needed for steemd process: %0.2f Gb" % (steemd_memory_need/bytes_per_gb))
 max_block_size = 64 * 1024
 
-object_count = int(available_ram / max_block_size)
-global_shared_capacity = int(available_ram / 3)
+if(available_ram<steemd_memory_need):
+  print("\tYou can`t run 'steemd' on this host, you need to free at least %0.2f Gb RAM" % ((steemd_memory_need-available_ram)/bytes_per_gb))
+  sys.exit()
+
+object_count = int((available_ram-stemd_memory_usage) / max_block_size)
+global_shared_capacity = int((available_ram-stemd_memory_usage) / 3)
 write_buffer_size = 1073741824
 
 config = dict()
@@ -74,14 +83,26 @@ config["base"]["optimize_level_style_compaction"] = True
 config["base"]["optimize_level_style_compaction"] = True
 config["base"]["optimize_level_style_compaction"] = True
 
-print("Writting config file: '%s' with following calculated params...:\n" % config_file_name)
+
 print("\tobject_count: %s" % str(object_count))
 print("\tglobal_shared_capacity: %i" % global_shared_capacity)
-if(object_count<max_of_open_files):
-  print("The actual number of allowed open files is less than the suggested open count, please correct with:")
-  answer = input("Do you want to set the limit to %s ?" % object_count)
+print("\tmax_nr_of_open_files: %i" % max_nr_of_open_files)
 
+if(object_count>max_nr_of_open_files):
+  print("\nThe actual number of allowed open files (%i) is lower than the suggested object count (%i)!" % (max_nr_of_open_files, object_count))
+  answer = input("\tDo you want to set the limit to %i (object_count increased by an offset of %s? (Y/N) " % (object_count + open_files_offset, open_files_offset))
+  if(answer=="Y"):
+    print("\tSetting open file limit to: %i ..." % (object_count + open_files_offset))
+  else:
+    print("\tPlease set the value manually with: $>ulimit -n %i" % (object_count + open_files_offset))
+
+answer = input("\tDo you want to save mira configuration file to %s " % config_file_name)
+if(answer=="Y"):
+  print("Writting config file: '%s' with following calculated params...:\n" % config_file_name)
 with open(config_file_name, 'w') as outfile:  
     json.dump(config, outfile, indent=4)
+
+
+
 
 
